3c95a17fc7193f376dd3e3707947e41bcc998edd
madz <you@example.com>
2017-02-09 22:52:41 -0500

    more changes

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = 5000;
+var port = process.env.PORT || 5000;
 
 server.listen(port);
 
 app.use(express.static('public'));
-
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/public/index.html');
-// });
 
 io.on('connection', function(socket) {
     console.log("connected");
