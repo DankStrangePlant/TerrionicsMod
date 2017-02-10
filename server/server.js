12326dbff64043bfed2b6280b3d7e883e941f24c
madz <you@example.com>
2017-02-09 22:34:38 -0500

    messing with the UI

--- server/server.js
+++ server/server.js
@@ -1,22 +1,29 @@
-﻿var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = 5000;
 
 server.listen(port);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static('public'));
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
 
 io.on('connection', function (socket) {
   console.log("connected");
   socket.on('SocketTest', function (data) {
     console.log(data);
   });
+  socket.on('disconnect', function(){
+        console.log('disconnected');
+    });
   socket.on('socket-connected', function (data) {
     io.sockets.emit('news', data);
   });
+
   socket.on('chat message', function (data) {
 	if(data.substring(0, "Spawn ".length) == "Spawn ")
 		io.sockets.emit('spawn item', data.substring("Spawn ".length, data.length));
